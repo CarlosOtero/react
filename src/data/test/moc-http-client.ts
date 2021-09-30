443c540aa4370b279a0ca236e72d2c23061dd774
Carlos Alberto Otero <email.otero@gmail.com>
2021-09-30 10:12:47 -0300

    refactor: change httpPostClient to receice an object

--- src/data/test/moc-http-client.ts
+++ src/data/test/moc-http-client.ts
@@ -1,9 +1,9 @@
-import { IHttpPostClient } from '../protocols/http/http-post-client'
+import { HttpPostParams, IHttpPostClient } from '../protocols/http/http-post-client'
 
 export class HttpPostClientSpy implements IHttpPostClient {
   url?: string
-  async post (url: string): Promise<void> {
-    this.url = url
+  async post (params: HttpPostParams): Promise<void> {
+    this.url = params.url
     return Promise.resolve()
   }
 }
