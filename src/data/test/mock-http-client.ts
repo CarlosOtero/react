f2e5e17ca813e5d65e7a27710b5584586f8421b1
Carlos Alberto Otero <email.otero@gmail.com>
2021-10-08 14:46:09 -0300

    refactor: group imports in index files

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,5 +1,4 @@
-import { HttpPostParams, IHttpPostClient } from '@/data/protocols/http/http-post-client'
-import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
+import { HttpPostParams, IHttpPostClient, HttpResponse, HttpStatusCode } from '@/data/protocols/http'
 
 export class HttpPostClientSpy<T, R> implements IHttpPostClient<T, R> {
   url?: string
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,9 +1,7 @@
-import { IHttpPostClient } from '@/data/protocols/http/http-post-client'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
-import { UnexpectedError } from '@/domain/errors/unexpescted-error'
-import { AccountModel } from '@/domain/models/account-model'
-import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
+import { IHttpPostClient, HttpStatusCode } from '@/data/protocols/http'
+import { Authentication, AuthenticationParams } from '@/domain/usecases'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { AccountModel } from '@/domain/models'
 
 export class RemoteAuthentication implements Authentication {
   constructor (
