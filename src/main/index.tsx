5bd505be16f147a87af3c941f88b08722a6dc8e6
Carlos Alberto Otero <email.otero@gmail.com>
2021-11-08 16:15:18 -0300

    feat: add app router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Login } from '@/presentation/pages/'
+import { Router } from '@/presentation/components/'
+import '@/presentation/styles/global.scss'
 
 ReactDOM.render(
-  <Login />,
+  <Router />,
   document.getElementById('main')
 )
