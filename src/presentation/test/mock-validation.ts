0b0178d5aa7669fdbcf3bb1923d9fc56bb6ffd0d
Carlos Alberto Otero <email.otero@gmail.com>
2021-11-09 15:41:15 -0300

    feat: ensure Login present erro if password is invalid

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -1,12 +1,8 @@
 import { IValidation } from '@/presentation/protocols/validation'
 
-export class ValidationSpy implements IValidation {
+export class ValidationStub implements IValidation {
   errorMessage: string;
-  fieldName: string;
-  fieldValue: string;
   validate (fieldName: string, fieldValue: string): string {
-    this.fieldName = fieldName
-    this.fieldValue = fieldValue
     return this.errorMessage
   }
 }
