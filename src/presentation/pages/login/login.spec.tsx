a92293c0bcec35e219521815096ef36c284ebbfe
Carlos Alberto Otero <email.otero@gmail.com>
2021-11-08 21:35:24 -0300

    feat: ensure login start with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,9 +3,11 @@ import { render } from '@testing-library/react'
 import Login from './login'
 
 describe('Login Component', () => {
-  test('should not render spinner and error on start', () => {
+  test('should start with initial state', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
-    expect(errorWrap.childElementCount).toBe(0)
+    expect(errorWrap.childElementCount).toBe(0) // espea que não tenha nehum filho //
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true) // espea o botão esteja desabilitado //
   })
 })
