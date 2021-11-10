5e90b53bc90f94e4b00f3e560e5a10ed7b3c070f
Carlos Alberto Otero <email.otero@gmail.com>
2021-11-10 12:08:09 -0300

    test: ensure RequiredFieldValidation returns false if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredFieldValidation } from './required-field-validation'
 import { RequiredFieldError } from '@/validation/errors'
+import faker from 'faker'
 
 describe('RequiredFieldValidation', () => {
   test('Shoud return error if field is empty', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Shoud return false if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
