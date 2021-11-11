e7c6efa7218defd8adafcd597b617b7afe8e3792
Carlos Alberto Otero <email.otero@gmail.com>
2021-11-10 23:01:48 -0300

    fix: correct spelling from MinLengthValidation

--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -1,7 +1,7 @@
 import { InvalidFieldError } from '@/validation/errors'
-import { IFielValidation } from '@/validation/protocols/field-validation'
+import { IFieldValidation } from '@/validation/protocols/field-validation'
 
-export class MinLengthValidation implements IFielValidation {
+export class MinLengthValidation implements IFieldValidation {
   constructor (readonly field: string, private readonly minLength: number) {}
   //
   validate (value: string): Error {
