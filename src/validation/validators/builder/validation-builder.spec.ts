8621739866a606dd2c3e855d177f57b77b5ae33a
Carlos Alberto Otero <email.otero@gmail.com>
2021-11-10 23:09:29 -0300

    test: ensure ValidationBuilder has an email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -6,4 +6,9 @@ describe('ValidationBuilder', () => {
     const validations = ValidationBuilder.field('any_field').required().build()
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
   })
+
+  test('Should return EmailValidation ', () => {
+    const validations = ValidationBuilder.field('any_field').email().build()
+    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
 })
