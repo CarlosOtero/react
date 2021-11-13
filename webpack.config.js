14aaf79c16701d9a01fb6cbbab6b55c8309bb91a
Carlos Alberto Otero <email.otero@gmail.com>
2021-11-13 20:25:13 -0300

    fix: correct version of sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -39,8 +39,10 @@ module.exports = {
     }]
   },
   devServer: {
-    contentBase: './public',
-    writeToDisk: true,
+    static: './public',
+    devMiddleware: {
+      writeToDisk: true
+    },
     historyApiFallback: true
   },
   externals: {
