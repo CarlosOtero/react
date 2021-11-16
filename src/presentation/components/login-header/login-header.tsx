4c25c7340681aafd369d8fe84593f9f83aae16d8
Carlos Alberto Otero <email.otero@gmail.com>
2021-11-16 15:30:11 -0300

    fix: loading images

--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -1,12 +1,10 @@
 import React, { memo } from 'react'
 import Styles from './login-header-styles.scss'
-import Logo from '@/presentation/components/logo/logo'
 
 const LoginHeader: React.FC = () => {
   return (
     <header className={Styles.header}>
-      <Logo />
-      <h1></h1>
+      <div className={Styles.img_logo}></div>
     </header>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,19 @@ module.exports = {
       loader: 'ts-loader',
       exclude: /node_modules/
     },
+
+    {
+      test: /\.(png|jpg|gif)$/i,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192
+          }
+        }
+      ]
+    },
+
     {
       test: /\.scss$/,
       use: [{
