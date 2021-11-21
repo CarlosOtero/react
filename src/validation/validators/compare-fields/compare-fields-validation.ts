15e03a021671574d94bb3d77e9b2f63682a2f6bb
CarlosOtero <email.otero@gmail.com>
2021-11-21 16:07:42 -0300

    feat: ensure CompareFieldsValidation returns error if validation fails

---
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -0,0 +1,12 @@
+import { IFieldValidation } from '@/validation/protocols/field-validation'
+import { InvalidFieldError } from '@/validation/errors'
+
+export class CompareFieldsValidation implements IFieldValidation {
+  constructor (
+    readonly field: string,
+    private readonly valueToCompare: string) {}
+
+  validate (value: string): Error {
+    return new InvalidFieldError()
+  }
+}
