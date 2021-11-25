3bb41490322723929ebe054d651797fb6550a410
CarlosOtero <email.otero@gmail.com>
2021-11-25 17:03:59 -0300

    feat: ensure LocalSaveAccessToken throws if no accessToken is provided

--- src/data/usecases/authentication/save-access-token/local-save-access-token.ts
+++ src/data/usecases/authentication/save-access-token/local-save-access-token.ts
@@ -1,9 +1,13 @@
 import { ISetStorage } from '@/data/protocols/cache/set-storage'
+import { UnexpectedError } from '@/domain/errors'
 import { ISaveAccessToken } from '@/domain/usecases/save-access-token'
 
 export class LocalSaveAccessToken implements ISaveAccessToken {
   constructor (private readonly setStorqge: ISetStorage) {}
   async save (accessToken: string): Promise<void> {
+    if (!accessToken) {
+      throw new UnexpectedError()
+    }
     await this.setStorqge.set('accessToken', accessToken)
   }
 }
