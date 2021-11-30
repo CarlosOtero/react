c5280c5b021fd7b2893bdc9f597fb7efc899584d
CarlosOtero <email.otero@gmail.com>
2021-11-30 17:04:40 -0300

    fix: correct min-length validation

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -20,7 +20,7 @@ describe('MinLengthValidation', () => {
   })
 
   test('Should return falsy if field does not exists in schema', () => {
-    const sut = makeSut(faker.database.column())
+    const sut = makeSut('any_field')
     const error = sut.validate({ [faker.database.column()]: faker.random.alphaNumeric(5) })
     expect(error).toBeFalsy()
   })
