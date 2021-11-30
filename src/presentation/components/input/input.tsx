3f36d965c8178784ce6b6ede67768698348e8763
CarlosOtero <email.otero@gmail.com>
2021-11-29 21:28:54 -0300

    reactor: improve input layout

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -10,28 +10,27 @@ const Input: React.FC<Props> = (props: Props) => {
   const error = state[`${props.name}Error`]
 
   return (
-    <div className={Styles.inputWrap}>
-
+    <div
+      data-testid={`${props.name}-wrap`}
+      className={Styles.inputWrap}
+      data-status={error ? 'invalid' : 'valid'}
+    >
       <input
         {...props}
         ref={inputRef}
+        title={error}
         placeholder = " "
         data-testid={props.name}
         readOnly
         onFocus={ e => { e.target.readOnly = false } }
         onChange={ e => { setState({ ...state, [e.target.name]: e.target.value }) } }
       />
-      <label onClick={() => { inputRef.current.focus() } }>
+      <label
+        data-testid={`${props.name}-label`}
+        title={error}
+        onClick={() => { inputRef.current.focus() } }>
         {props.placeholder}
       </label>
-      <span
-        data-testid={`${props.name}-status`}
-        title={error || 'Tudo certo!'}
-        className={Styles.status}
-      >
-        {error ? '🔴' : '🟢'}
-      </span>
-
     </div>
   )
 }
