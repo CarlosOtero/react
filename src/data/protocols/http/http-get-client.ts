46c85b2a668fbc9890343490bf88581030239b76
CarlosOtero <email.otero@gmail.com>
2021-12-02 16:48:37 -0300

    feat: ensure RemoteLoadSurveyList throws UnespectedError on 403

--- src/data/protocols/http/http-get-client.ts
+++ src/data/protocols/http/http-get-client.ts
@@ -1,7 +1,9 @@
+import { HttpResponse } from '.'
+
 export type HttpGetParams = {
   url: string
 }
 
-export interface IHttpGetClient {
-  get: (params: HttpGetParams) => Promise<void>
+export interface IHttpGetClient<R = any> {
+  get: (params: HttpGetParams) => Promise<HttpResponse<R>>
 }
