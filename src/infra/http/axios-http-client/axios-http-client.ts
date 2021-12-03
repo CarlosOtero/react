27acaf03f4152a51076c223a3ffc7520858429b8
CarlosOtero <email.otero@gmail.com>
2021-12-03 16:45:08 -0300

    feat: ensure AxiosHttpClient returns correvt value on failure

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -16,7 +16,12 @@ export class AxiosHttpClient implements IHttpPostClient, IHttpGetClient {
   }
 
   async get (params: HttpGetParams): Promise<HttpResponse> {
-    const axiosResponse = await axios.get(params.url)
+    let axiosResponse
+    try {
+      axiosResponse = await axios.get(params.url)
+    } catch (error) {
+      axiosResponse = error.response
+    }
     return {
       statusCode: axiosResponse.status,
       body: axiosResponse.data
