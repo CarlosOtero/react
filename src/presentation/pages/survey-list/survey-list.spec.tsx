3707ef36483736dc796e2bca77baab3733e1daf2
CarlosOtero <email.otero@gmail.com>
2021-12-07 12:25:51 -0300

    test: ensure SurveyList component calss LoadSurveyList

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,9 +1,27 @@
 import { SurveyList } from '@/presentation/pages'
-import React from 'react'
+import { ILoadSureyList } from '@/domain/usecases'
+import { SurveyModel } from '@/domain/models'
 import { render, screen } from '@testing-library/react'
+import React from 'react'
 
-const makeSut = (): void => {
-  render(<SurveyList />)
+class LoadSurveyListSpy implements ILoadSureyList {
+  callsCount = 0
+  async loadAll (): Promise<SurveyModel[]> {
+    this.callsCount++
+    return []
+  }
+}
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
+  return {
+    loadSurveyListSpy
+  }
 }
 
 describe('SurveyList Component', () => {
@@ -11,5 +29,10 @@ describe('SurveyList Component', () => {
     makeSut()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+  })
+
+  test('Should call LoadSurveyList (usecase)', () => {
+    const { loadSurveyListSpy } = makeSut()
+    expect(loadSurveyListSpy.callsCount).toBe(1)
   })
 })
