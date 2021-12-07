d7c3fcd00c39ce770bacd00d3124019dfec3f6a5
CarlosOtero <email.otero@gmail.com>
2021-12-07 20:38:21 -0300

    feat: ensure SurveyList render error on failure

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -11,13 +11,15 @@ type Props = {
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
   const [state, setState] = useState({
-    surveys: [] as SurveyModel[]
+    surveys: [] as SurveyModel[],
+    error: ''
   })
 
   useEffect(() => {
     (async function () {
       loadSurveyList.loadAll()
-        .then(surveys => setState({ surveys }))
+        .then(surveys => setState({ ...state, surveys }))
+        .catch(error => setState({ ...state, error: error.message }))
     })()
   }, [])
 
@@ -26,12 +28,18 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        <ul data-testid="survey-list">
-          { state.surveys.length
-            ? state.surveys.map((survey: SurveyModel) => <SurveyItem key={survey.id} survey={survey} />)
-            : <SurveyItemEmpty />
-          }
-        </ul>
+        {state.error
+          ? <div>
+            <span data-testid="error">{state.error}</span>
+            <button>Recarregar</button>
+          </div>
+          : <ul data-testid="survey-list">
+            { state.surveys.length
+              ? state.surveys.map((survey: SurveyModel) => <SurveyItem key={survey.id} survey={survey} />)
+              : <SurveyItemEmpty />
+            }
+          </ul>
+        }
       </div>
       <Footer />
     </div>
