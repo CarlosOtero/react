98cc7d3c59583d24a379b92c25ab769d5136ea42
CarlosOtero <email.otero@gmail.com>
2021-12-09 20:39:33 -0300

    fix: move files to its corrects folders

--- src/domain/test/mock-add-account.ts
+++ src/domain/test/mock-add-account.ts
@@ -1,4 +1,7 @@
-import { AddAccountParams } from '@/domain/usecases'
+import { AddAccountParams, IAddAccount } from '@/domain/usecases'
+import { mockAccountModel } from '@/domain/test'
+import { AccountModel } from '../models/account-model'
+
 import faker from 'faker'
 
 export const mockAddAccountParams = (): AddAccountParams => {
@@ -8,5 +11,17 @@ export const mockAddAccountParams = (): AddAccountParams => {
     email: faker.internet.email(),
     password: password,
     passwordConfirmation: password
+  }
+}
+
+export class AddAccountSpy implements IAddAccount {
+  account = mockAccountModel()
+  params: AddAccountParams
+  callsCount = 0
+  //
+  async add (params: AddAccountParams): Promise<AccountModel> {
+    this.params = params
+    this.callsCount++
+    return this.account
   }
 }
