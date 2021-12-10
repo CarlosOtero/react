8b724928d0ee1d1d0c25437bd972e458198ccaaf
CarlosOtero <email.otero@gmail.com>
2021-12-10 17:50:30 -0300

    feat: ensure reader renders username correctly

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -6,7 +6,7 @@ import { useHistory } from 'react-router-dom'
 
 const Header: React.FC = () => {
   const history = useHistory()
-  const { setCurrentAccount } = useContext(ApiContext)
+  const { setCurrentAccount, getCurrentAccount } = useContext(ApiContext)
   const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
     event.preventDefault()
     setCurrentAccount(undefined)
@@ -18,7 +18,7 @@ const Header: React.FC = () => {
       <div className={Styles.headerContent}>
         <Logo />
         <div className={Styles.logoutWrap}>
-          <span>Carlos Alberto</span>
+          <span data-testid="username">{getCurrentAccount().name}</span>
           <a data-testid="logout" href="#" onClick={logout}>Sair</a>
         </div>
       </div>
